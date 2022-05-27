c055953890ff137c91ecfe0d37ad9579cb04c4e0
fabian castaño ortiz <fabian9433@gmail.com>
2022-05-27 07:55:19 -0500

    prisma example

--- server/functions/api.js
+++ server/functions/api.js
@@ -51,6 +51,7 @@ router.get("/tickets", (req, res) => {
 });
 
 app.use(`/.netlify/functions/api`, router);
+app.use(express.json);
 
 module.exports = app;
 module.exports.handler = serverless(app);
