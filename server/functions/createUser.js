5ad1b84de42395a6315065f8c77616f435ee955c
fabian castaño ortiz <fabian9433@gmail.com>
2022-05-27 00:52:44 -0500

    prisma example

--- server/functions/createUser.js
+++ server/functions/createUser.js
@@ -12,19 +12,10 @@ exports.handler = async (event, context, callback) => {
       body: JSON.stringify(createdUser),
     };
   } catch (e) {
-    if (e instanceof PrismaClientRequestError) {
-      if (e.code === "P2002") {
-        return {
-          statusCode: 409,
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({
-            error: "A user with this email already exists",
-          }),
-        };
-      }
-    }
-
-    console.error(e);
-    return { statusCode: 500 };
+    return {
+      statusCode: 409,
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(e),
+    };
   }
 };
