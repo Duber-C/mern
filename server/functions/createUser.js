ce936c2137aa67211e7e0fe6fba52070c0e11822
fabian castaño ortiz <fabian9433@gmail.com>
2022-05-27 00:38:49 -0500

    prisma example

--- server/functions/createUser.js
+++ server/functions/createUser.js
@@ -1,30 +1,30 @@
-const { PrismaClient, PrismaClientRequestError } = require('@prisma/client')
-const prisma = new PrismaClient()
+const { PrismaClient, PrismaClientRequestError } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 exports.handler = async (event, context, callback) => {
   try {
-    const data = JSON.parse(event.body)
-    const createdUser = await prisma.user.create({ data })
+    const data = JSON.parse(event.body);
+    const createdUser = await prisma.user.create({ name: "asdfasd" });
 
     return {
       statusCode: 200,
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(createdUser)
-    }
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(createdUser),
+    };
   } catch (e) {
     if (e instanceof PrismaClientRequestError) {
-      if (e.code === 'P2002') {
+      if (e.code === "P2002") {
         return {
           statusCode: 409,
-          headers: { 'Content-Type': 'application/json' },
+          headers: { "Content-Type": "application/json" },
           body: JSON.stringify({
-            error: 'A user with this email already exists'
-          })
-        }
+            error: "A user with this email already exists",
+          }),
+        };
       }
     }
 
-    console.error(e)
-    return { statusCode: 500 }
+    console.error(e);
+    return { statusCode: 500 };
   }
-}
+};
