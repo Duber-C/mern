65d10078fce411562e4a7fe49853b3ed6b771d26
fabian castaño ortiz <fabian9433@gmail.com>
2022-05-27 07:29:37 -0500

    prisma example

--- server/functions/api.js
+++ server/functions/api.js
@@ -27,21 +27,25 @@ router.post("/testpost", (req, res) => {
 
 router.get("/tickets", (req, res) => {
   (async () => {
-    // await prisma.$connect();
+    await prisma.$connect().catch((err) => {
+      res.json(err);
+    });
 
-    const allUsers = await prisma.user.findMany({
-      include: {
-        notifications: true,
-        tickets: {
-          select: {
-            description: true,
-            priority: true,
-            date: true,
-            customer: true,
+    const allUsers = await prisma.user
+      .findMany({
+        include: {
+          notifications: true,
+          tickets: {
+            select: {
+              description: true,
+              priority: true,
+              date: true,
+              customer: true,
+            },
           },
         },
-      },
-    });
+      })
+      .catch((err) => res.json(err));
     res.json(allUsers);
   })();
 });
